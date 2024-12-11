6b576cf03f1c3212487210644ad606799c9c79d2
Lospub <zhjyy002@gmail.com>
2024-12-11 13:04:46 -0700

    remove tags api endpoint test code

--- routes/tags.js
+++ routes/tags.js
@@ -7,19 +7,10 @@ const tags = JSON.parse(
     )
 );
 
-const app = express();
-const PORT = 8080;
-
 const router = express.Router();
 
 router.get('/tags', (req, res) => {
-    console.log("get tags");
     res.json(tags);
 });
 
-
-app.use('/', router);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default router;
