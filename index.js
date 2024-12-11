1c52c8f34cea422650fb83fb374a6120bc7e4c32
Lospub <zhjyy002@gmail.com>
2024-12-11 10:52:00 -0700

    basic test for get request

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from "express";
+
 const app = express();
 const port = process.env.PORT || process.argv[2] || 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ name: "Eden" });
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
