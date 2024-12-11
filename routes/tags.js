017a135672233aff3f4c6a3b213a140a0269e242
Lospub <zhjyy002@gmail.com>
2024-12-11 13:01:47 -0700

    resolve import JSON warning

--- routes/tags.js
+++ routes/tags.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import tags from '../data/tags.json' with { type: "json" };
+import { readFile } from 'fs/promises';
+
+const tags = JSON.parse(
+    await readFile(
+        new URL('../data/tags.json', import.meta.url)
+    )
+);
 
 const app = express();
 const PORT = 8080;
@@ -8,7 +14,7 @@ const router = express.Router();
 
 router.get('/tags', (req, res) => {
     console.log("get tags");
-    res.json(tags); 
+    res.json(tags);
 });
 
 
