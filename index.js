71b10b2eb006b369dbe534aadd8b553d061dba0c
Lospub <zhjyy002@gmail.com>
2024-12-13 10:34:08 -0700

    Change Photo to static photo data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import cors from "cors"
 const app = express();
 const port = process.env.PORT || process.argv[2] || 8080;
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
